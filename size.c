4fbaca50596809f8a1b85416f75e1c749ac62e6f
Minah4code <aminahmuhammed5@gmail.com>
2023-06-02 14:59:14 -0700

    size.c

---
+++ size.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_size - Parses the size field from the format string
+ * @format: The format string
+ * @i: Pointer to the current index in the format string
+ * @va: The va_list containing the variable arguments
+ *
+ * Return: The size value
+ */
+int get_size(const char *format, int *i, va_list va)
+{
+	int size = 0;
+
+	if (format[*i] == '*')
+	{
+		size = va_arg(va, int);
+		(*i)++;
+	}
+	else
+	{
+		while (_isdigit(format[*i]))
+		{
+			size = (size * 10) + (format[*i] - '0');
+			(*i)++;
+		}
+	}
+	return (size);
+}
